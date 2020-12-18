c755807b43781d8b7563974fc177a79211e68013
=qmrmqmrm <parkmingyu11@gmail.com>
2020-12-18 21:12:36 +0900

    modify structure

--- src/camera_config.py
+++ src/camera_config.py
@@ -5,77 +5,77 @@ import dynamic_reconfigure.client
 import numpy as np
 from std_msgs.msg import Float32
 
-class Camera_Parameter:
 
+class CameraDefult:
+    exposure = 0.7
+    shutter = 0.01
+    logshut = -2.
+    gain = 0.
+    framerate = 7
+
+
+class CameraParams:
+    exposure = CameraDefult.exposure
+    shutter = CameraDefult.shutter
+    logshut = CameraDefult.logshut
+    gain = CameraDefult.gain
+    framerate =CameraDefult.framerate
+
+
+class CameraAutoAdjuster:
     def __init__(self):
-        self.sub = rospy.Subscriber("ref",Float32,self.ref_callback)
-        self.ref = 0.0
-        self.shutter = 0.03
-        self.client = dynamic_reconfigure.client.Client("/camera/camera_nodelet", timeout=30, config_callback=self.callback)
-        
+        self.sub = rospy.Subscriber("ref", Float32, self.ref_callback)
+        self.ref = 100
+        self.client = dynamic_reconfigure.client.Client("/camera/camera_nodelet", timeout=30,
+                                                        config_callback=self.callback)
+        self.target = 200
+        self.pE = -0.7
+        self.pL = 0.01
+
     def change_config(self):
-        ref = self.ref
-        gain = 0
-        framrate =10 
-        target = 120
-        b =False
-        
-        pE = 1
-        r = rospy.Rate(0.1)
-        pL =0.01
-        exposure = 0.7
-        #print("self.shutter",self.shutter * 1000)
-        shutter = self.shutter * 1000
-        err = target-ref
-        shutter += pL * err
-        #print("self.shutter",shutter)
-        logshut = np.log(shutter)
-        #print(logshut)
+        err = self.target - self.ref
+        CameraParams.logshut += self.pL * err
+        CameraParams.logshut = max(min(CameraParams.logshut, -1), -4)
+        CameraParams.shutter = np.exp(CameraParams.logshut)
+        if CameraParams.logshut < -3:
+            CameraParams.exposure = (CameraParams.logshut + 3) * self.pE
+        elif CameraParams.logshut >= -1:
+            CameraParams.exposure = (CameraParams.logshut + 2) * self.pE
+        else:
+            CameraParams.exposure = 0.7
 
-        
-        
-
-        logshut = max(min(logshut,2),-1)
-        #print(logshut)
-        if logshut < 0:
-            exposure = logshut * pE
-        if logshut >1:
-            exposure = (logshut -1) * pE
-        
-        shutter_speed = np.exp(logshut)*0.001
         rospy.loginfo("err:{}".format(err))
-        self.client.update_configuration({"auto_exposure":False,"exposure":exposure,"auto_shutter":False,"shutter_speed":shutter_speed,"auto_gain":False,"gain":0.0}) #   ,"shutter_speed":shutter
-    
-    
-    def ref_callback(self,data):
-        ref = data.data
-        self.ref = ref
+        config = {"auto_exposure": False,"exposure":CameraParams.exposure,
+                  "auto_shutter": False, "shutter_speed": CameraParams.shutter,
+                  "auto_gain": False, "gain": CameraParams.gain,
+                  "frame_rate": CameraParams.framerate}
+        print("set config:", config)
+        self.client.update_configuration(config)
 
+    def ref_callback(self, data):
+        self.ref = data.data
+        print("ref value:", self.ref)
 
-    def callback(self,config):
-        #rospy.loginfo("Config test")
-        #rospy.loginfo(config)
-        
-        rospy.loginfo("auto_exposure :{auto_exposure},exposure: {exposure}, auto_shutter: {auto_shutter}, shutter_speed: {shutter_speed}, gain: {gain}".format(**config))
-        self.shutter= config["shutter_speed"]
+    def callback(self, config):
+        rospy.loginfo("auto_exposure :{auto_exposure},exposure: {exposure}, "
+                      "auto_shutter: {auto_shutter}, shutter_speed: {shutter_speed},"
+                      " gain: {gain}".format(**config))
+        # CameraParams.shutter = config["shutter_speed"]
+        # CameraParams.logshut = np.log(CameraParams.shutter)
+
 
 def main():
     rospy.init_node("dynamic_client")
-    param = Camera_Parameter()
-    
-    
+    param = CameraAutoAdjuster()
     r = rospy.Rate(10)
-
-    #b = False
+    # b = False
     while not rospy.is_shutdown():
         param.change_config()
         r.sleep()
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
         main()
     except rospy.ROSInterruptException:
         pass
-    
